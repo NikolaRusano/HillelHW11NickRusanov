587cdf3d92daee7fcfbb62b4268d45c0eb75616d
mrusanov <nikolarusano92@gmail.com>
2021-06-07 22:35:03 +0300

    3rd task

--- src/Task1/Task2.java
+++ src/Task1/Task2.java
@@ -3,22 +3,29 @@ package Task1;
 import java.util.Scanner;
 
 public class Task2 {
-    public   String   withoutFirstAndLastChar(String  inputString)   {
-        String resString="";
-        try{
+    public String withoutFirstAndLastChar(String inputString) {
+        String resString = "";
+        try {
             char[] inputStringChar = inputString.toCharArray();
-            char[] tmpCharArr = new char[inputStringChar.length-2];
-            for(int i = 1; i < inputStringChar.length-1; i++){
-                tmpCharArr[i-1] = inputStringChar[i];
+            char[] tmpCharArr = new char[inputStringChar.length - 2];
+
+           /* for(int i = 1; i < inputStringChar.length-1; i++){
+                tmpCharArr[i-1] = inputStringChar[i];*/
+            //System.out.println(tmpCharArr[i-1]);
+
+            //System.out.println(resString += tmpCharArr.toString());
+            System.out.println(resString = inputString.substring(1, inputString.length() - 1));
+            if (resString.length() == 0) {
+                System.out.println("the word is too short there is only 2 char for cut off");
             }
-            resString = tmpCharArr.toString();
-        }catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
             System.out.println("The string is too short and can't be cut off with first and last characters");
-        }catch (NegativeArraySizeException e){
+        } catch (NegativeArraySizeException e1) {
 
-            System.out.println("Inncorrect string len for cut off");
-            System.out.println(e.getMessage());
+            System.out.println("Incorrect string len for cut off");
+            System.out.println(e1.getMessage());
+
         }
 
         return resString;
@@ -29,12 +36,8 @@ public class Task2 {
         System.out.println("Input string for remove first and last character");
         String inputString = scanner.nextLine();
         Task2 t2 = new Task2();
-        t2.withoutFirstAndLastChar(inputString);
+        System.out.println(t2.withoutFirstAndLastChar(inputString));
     }
-
-
-
-
 
 
 }
---
+++ src/Task1/Task3.java
@@ -0,0 +1,26 @@
+package Task1;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+    public String repeatRepeatRepeat(String inputString) {
+        String[] tempString = new String[3];
+
+        
+        String[] resString = {""};
+
+        return resString;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        String[] tempString = inputString.substring(0,2);
+        System.out.println(tempString);
+    }
+}
