3328cc22efaf205c5a79c99c4ab7ec6906e47951
mrusanov <nikolarusano92@gmail.com>
2021-06-08 20:57:18 +0300

    5th task finished

--- src/Task5/Ball.java
+++ src/Task5/Ball.java
@@ -1,32 +1,46 @@
 package Task5;
 
-public class Ball {
-    enum  size{
-        5.0,
-        3.0,
-        6.0,
-        7.0;
+public enum Ball {
+    Wilson(7.0,"basketball",823.0,"rubber"),
+    Reflex(5.0,"football",339.0,"polyvinyl Chloride"),
+    ExtremeMotion(5.0,"volleyball",229.0,"polyvinyl Chloride"),
+    Nike(7.0,"basketball",1197.0,"Synthetic Leather"),
+    Select(5.0,"American football",480.0,"rubber");
+
+    double size, price;
+    String sportKind, material;
+
+    Ball(double size, String sportKind,double price, String material) {
+        this.size = size;
+        this.price = price;
+        this.sportKind = sportKind;
+        this.material = material;
     }
 
-    enum sportKind{
-        Football,
-        Basketball,
-        Volleyball,
-        WaterPolo;
-
-    }
-    enum price{
-        300.0,
-        1000.0,
-        500.0,
-        700.0;
-    }
-    enum Material{
-        leather,
-        cheapImitation;
+    public double getSize() {
+        return size;
     }
 
+    public double getPrice() {
+        return price;
+    }
 
+    public String getSportKind() {
+        return sportKind;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    @Override
+    public String toString(){
+        return "Ball{" +
+                " Size: " + size +
+                " Sport kind: " + sportKind +
+                " price" + price +
+                " Material: " + material + '\'' + '}';
+    }
 
 
 }
---
+++ src/Task5/BallTester.java
@@ -0,0 +1,16 @@
+package Task5;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class BallTester {
+
+    public static void main(String[] args) {
+        System.out.println(Ball.Wilson);
+        System.out.println(Ball.Reflex);
+        System.out.println(Ball.ExtremeMotion);
+        System.out.println(Ball.Nike);
+        System.out.println(Ball.Select);
+
+    }
+
+}
